c94a5fca7eee52b796a115c15cf2c3d9c2f0b022
Matias Bastos <matias.bastos@gmail.com>
2015-09-15 17:27:55 -0300

    Added db classes definition.

--- main.py
+++ main.py
@@ -1,25 +1,36 @@
-from flask import Flask
+from flask import Flask, session
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/ascentio-test.db'
 db = SQLAlchemy(app)
 
-
 class Session(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True)
 
-#class Operation(db.Model): 
-#    pass
+    def __init__(self, name):
+        self.name  = name
 
-@app.route("/")
-def main():
-    return "hola"
+    def __repr__(self):
+        return "Session: %s" % self.name
 
-@app.route("/test/<path:aaa>")
-def a_test_function(aaa):
-   return "asdasdsa %s" % aaa
+class Operation(db.Model): 
+    id = db.Column(db.Integer, primary_key=True)
+    session_id = db.Column(db.Integer, db.ForeignKey('session.id'))
+    session = db.relationship('Session', backref=db.backref('operations', lazy='dynamic'))
+    input = db.Column(db.String(255))
+    output = db.Column(db.String(64))
+
+    def __init__(self, session, input, output):
+        self.session = session
+        self.input = input
+        self.output = output
+
+    def __repr__(self):
+        return "Operation %s: Input = %s, Output = %s" % (self.id, self.input, self.output)
+    
+app.secret_key = 'aafeb9552c67474e9b6ce85f0394aab1'
 
 if __name__ == "__main__":
     db.create_all()
